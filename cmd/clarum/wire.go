13031c37b24f36e636c812d49a1185014936909c
Anthony Lee <alee792@gmail.com>
2019-03-10 12:15:39 -0500

    push

--- cmd/clarum/wire.go
+++ cmd/clarum/wire.go
@@ -1,4 +1,4 @@
-// +build wireinject
+//+build wireinject
 
 package main
 
@@ -6,9 +6,40 @@ import (
 	"github.com/alee792/clarum/pkg/clarum"
 	"github.com/alee792/clarum/pkg/storage/bolt"
 	"github.com/google/wire"
+	"go.etcd.io/bbolt"
+	"go.uber.org/zap"
 )
 
-func initClarum(db *bolt.Repo) *clarum.Server {
-	wire.Build(bolt.NewRepo)
+// Config for Resolver.
+type Config struct {
+	Bolt struct{ 
+		Path string
+		Opts bbolt.Options
+	}
+}  
+ 
+// Resovler of dependencies.
+type Resolver struct { 
+	bolt *bbolt.DB
+}   
+
+func provideBolt() (*bbolt.DB, err)  {
+	if r.bolt == nil {
+		db ,err := bbolt.Open(r.Config.Bolt.Path, 0600, r.Config.Bolt.Opts)
+		if err != nil {
+			return nil, err
+		} 
+		r.bolt = db  
+	} 
+	return r.bolt, nil
+}
+
+func initClarum() *clarum.Server {
+	wire.Build(
+		zap.NewDevelopment,
+		bolt.NewRepo,
+		wire.Bind((*clarum.Repo)(nil), (*bolt.Repo)(nil)),
+		clarum.NewServer,
+	)
 	return &clarum.Server{}
 }
